85d5c70e94ff8e61a26d857904dbfc8bc85cc8ec
Andrew Taranenko <andrushkataranenko@gmail.com>
2023-02-12 21:18:51 +0200

    12.02.2023

---
+++ Lab 1/task4.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# -- Sheet --
+
+# Here is the sample mean:
+# <div align="right">$\bar{y}=\frac{1}{n}\displaystyle\sum_{i=1}^{n} y_i$</div>
+# <br/>
+# Here is the sample variance:
+# <div align="right">$σ^{2}=\frac{\displaystyle\sum_{i=1}^{n} (y_i -\bar{y})^{2}}{n-1}$</div>
+# <br/>
+# Here is the sample standart deviation:
+# <div align="right">$σ=\sqrt{\frac{\displaystyle\sum_{i=1}^{n} (y_i -\bar{y})^{2}}{n-1}}$</div>
+
+
